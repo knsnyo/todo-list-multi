6c8a74bc52837b40feb3d8f8c7c5613529779ee0
knsnyo <isc963@naver.com>
2023-06-27 12:40:09 +0900

    20230627 notifier...

---
+++ swift-todo/swift-todo/swift_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todoApp.swift
+//  swift-todo
+//
+//  Created by knsn on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
